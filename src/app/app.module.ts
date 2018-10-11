c6b24899403dfbc28d3c70775bdc16c50e520097
danielakujawa <kujawa.daniela@gmail.com>
2018-10-11 11:49:26 +0200

    project start and gnomes list page component created

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,12 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { GnomesListComponent } from './pages/gnomes-list/gnomes-list.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    GnomesListComponent
   ],
   imports: [
     BrowserModule
---
+++ src/app/pages/gnomes-list/gnomes-list.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-gnomes-list',
+  templateUrl: './gnomes-list.component.html',
+  styleUrls: ['./gnomes-list.component.scss']
+})
+export class GnomesListComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
